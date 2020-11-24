56173d282ddde151102a00ee9a573f50527ca76f
520MianXiangDuiXiang520 <15364968962@163.com>
2020-11-24 16:53:05 +0800

    :sparkles: 添加 cors 中间件

--- routes.go
+++ routes.go
@@ -3,11 +3,12 @@ package main
 import (
 	ginTools "github.com/520MianXiangDuiXiang520/GinTools/gin_tools"
 	"github.com/gin-gonic/gin"
+	"simple_ca/src/middleware"
 	"simple_ca/src/routes"
 )
 
 func Register(c *gin.Engine) {
-	// c.Use(middleware.ApiView(), middleware2.CorsHandler())
+	c.Use(middleware.CorsHandler())
 	ginTools.URLPatterns(c, "api/ca", routes.CARegister)
 	ginTools.URLPatterns(c, "api/auth", routes.AuthRegister)
 	ginTools.URLPatterns(c, "api/audit", routes.AuditRegister)
---
+++ src/middleware/cors.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	ginTools "github.com/520MianXiangDuiXiang520/GinTools/gin_tools"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func CorsHandler() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		origin := context.GetHeader("Origin")
+		method := context.Request.Method
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Origin", origin)
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		context.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,"+
+			"session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT,"+
+			" X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type,"+
+			" Pragma,token,openid,opentoken, cookies, Cookies, cookie, Cookies")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, "+
+			"Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar")
+		context.Header("Access-Control-Max-Age", "172800")
+		context.Header("Access-Control-Allow-Credentials", "true")
+		context.Set("content-type", "application/json")
+		// 设置返回格式是json
+
+		if method == "OPTIONS" {
+			context.Abort()
+			context.JSON(http.StatusOK, ginTools.SuccessRespHeader)
+		}
+		context.Next()
+	}
+}
